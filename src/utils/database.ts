e16ca31c5c460860007dfb2561c2c5b94e8b0fba
Octavian Okumu <octaviookumu@gmail.com>
2021-10-10 20:31:57 +0300

    Set up access.ts

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -13,7 +13,7 @@ export default class Database<T> {
     this.repo = getManager().getRepository(entityClass);
   }
 
-  async save(entity: T) {
+  async save(entity: T): Promise<boolean> {
     try {
       await this.repo.save(entity);
       return true;
@@ -23,10 +23,41 @@ export default class Database<T> {
     }
   }
 
-  async get(filter: object) {
+
+  // more efficient to do sql statement rather than calling state fxn when updating
+  async update(
+    table: string,
+    values: object,
+    filter: string
+  ): Promise<boolean> {
+    try {
+      const result = await this.repo
+        .createQueryBuilder(table)
+        .update()
+        .set(values)
+        .where(filter)
+        .execute();
+      return result.affected != undefined && result.affected > 0;
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
+  }
+
+  async get(filter: object): Promise<T | undefined> {
     try {
       // e.g. filter = { ukey: '123-abc' };
       return await this.repo.findOne(filter);
+    } catch (err) {
+      console.log(err);
+      return undefined;
+    }
+  }
+
+  async all(): Promise<T[] | undefined> {
+    try {
+      const rows = await this.repo.find();
+      return rows;
     } catch (err) {
       console.log(err);
       return undefined;
